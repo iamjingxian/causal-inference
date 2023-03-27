2fbac9c825fc83375cafa29eabfc1973b29fad79
russ <iamjingxian@gmail.com>
2023-03-27 13:12:45 +0800

    Update steroid_first_dose.sql

--- steroid_first_dose.sql
+++ steroid_first_dose.sql
@@ -1,7 +1,8 @@
 -- patients administered steroids; select for 1st dose in each hadm_id
+-- adds a column "steroid_first_dose_time" which is a duplicate of "starttime"; more intuitive to understand
 
 CREATE TABLE mimiciv_derived.steroid_first_dose AS
-SELECT *
+SELECT s1.*, s1.starttime AS steroid_first_dose_time
 FROM mimiciv_derived.steroid s1
 INNER JOIN (
   SELECT hadm_id, MIN(starttime) AS earliest_starttime
